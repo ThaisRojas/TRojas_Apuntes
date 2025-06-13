043840161cd3344c8d12e523ec70669f7c3790c3
Thais Rojas <thais.rojas@udla.edu.ec>
2025-06-12 22:48:55 -0500

    Agregar archivos de proyecto.

---
+++ TRojas_Apuntes/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TRojas_Apuntes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
