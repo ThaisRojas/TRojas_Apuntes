ede862ea138f855024984d9efceb51b6e5be32a1
Thais Rojas <thais.rojas@udla.edu.ec>
2025-06-14 22:48:15 -0500

    imagen agregada

--- TRojas_Apuntes/MainPage.xaml.cs
+++ TRojas_Apuntes/MainPage.xaml.cs
@@ -12,9 +12,10 @@ public partial class MainPage : ContentPage
     private void OnCounterClicked(object sender, EventArgs e)
     {
         count++;
-
-        CounterLabel.Text = "Has hecho clic {count} vez(es)";
-        SemanticScreenReader.Announce(CounterLabel.Text);
+        CounterLabel.Text = $"Has hecho clic {count} vez(es)";
     }
 }
+
+
+
 
